beb714d898cb2fb352bb026a9bb92b2a6aae8b73
Willian Alves <willian.alves@sap.com>
2018-09-14 19:47:23 -0300

    first commit

---
+++ src/Sequencial.java
@@ -0,0 +1,97 @@
+/**
+ Programa em Java correspondente à versão sequencial da multiplicação de matrizes.
+ @autor Roland Teodorowitsch
+ @version 4 set. 2018
+ */
+
+public class Sequencial {
+
+    // DIMENSOES DAS MATRIZES
+    public static final int SIZE = 2000;
+
+    // ESTRUTURAS DE DADOS COMPARTILHADA
+    public static int[][] m1;
+    public static int[][] m2;
+    public static int[][] mres;
+
+    public static void main(String[] args) {
+
+        for (int vezes = 100; vezes <= SIZE; vezes += 100) {
+
+            // INICIALIZA OS ARRAYS A SEREM MULTIPLICADOS
+            m1 = new int[vezes][vezes];
+            m2 = new int[vezes][vezes];
+            mres = new int[vezes][vezes];
+            if (m1[0].length != m2.length || mres.length != m1.length || mres[0].length != m2[0].length) {
+                System.err.println("Impossivel multiplicar matrizes: parametros invalidos.");
+                System.exit(1);
+            }
+            int k = 1;
+            for (int i = 0; i < vezes; i++) {
+                for (int j = 0; j < vezes; j++) {
+                    if (k % 2 == 0)
+                        m1[i][j] = -k;
+                    else
+                        m1[i][j] = k;
+                }
+                k++;
+            }
+            k = 1;
+            for (int j = 0; j < vezes; j++) {
+                for (int i = 0; i < vezes; i++) {
+                    if (k % 2 == 0)
+                        m2[i][j] = -k;
+                    else
+                        m2[i][j] = k;
+                }
+                k++;
+            }
+
+            // PREPARA PARA MEDIR TEMPO
+            long inicio = System.nanoTime();
+
+            // REALIZA A MULTIPLICACAO
+            for (int i = 0; i < mres.length; i++) {
+                for (int j = 0; j < mres[0].length; j++) {
+                    mres[i][j] = 0;
+                    for (k = 0; k < m2.length; k++) {
+                        mres[i][j] += m1[i][k] * m2[k][j];
+                    }
+                }
+            }
+
+            // OBTEM O TEMPO
+            long fim = System.nanoTime();
+            double total = (fim - inicio) / 1000000000.0;
+
+            // VERIFICA SE O RESULTADO DA MULTIPLICACAO ESTA CORRETO
+            for (int i = 0; i < vezes; i++) {
+                k = vezes * (i + 1);
+                for (int j = 0; j < vezes; j++) {
+                    int k_col = k * (j + 1);
+                    if (i % 2 == 0) {
+                        if (j % 2 == 0) {
+                            if (mres[i][j] != k_col)
+                                System.exit(1);
+                        } else {
+                            if (mres[i][j] != -k_col)
+                                System.exit(1);
+                        }
+                    } else {
+                        if (j % 2 == 0) {
+                            if (mres[i][j] != -k_col)
+                                System.exit(1);
+                        } else {
+                            if (mres[i][j] != k_col)
+                                System.exit(1);
+                        }
+                    }
+                }
+            }
+
+            // MOSTRA O TEMPO DE EXECUCAO
+            System.out.printf(vezes + ": %f\n", total);
+
+        }
+    }
+}
